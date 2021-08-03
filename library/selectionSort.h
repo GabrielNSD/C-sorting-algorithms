866a4cfbdfcb6b3ee7a4e185e7ec3371cdf341ba
GabrielNSD <gabrielnsdantas@gmail.com>
2021-08-03 16:38:15 -0300

    selection sort

---
+++ library/selectionSort.h
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include "./utils/utils.h"
+
+void selectionSortOP(int **v, int size)
+{
+    int *sorted = (int *)malloc(size * sizeof(int));
+    for (int i = 0; i < size; i++)
+    {
+        int iSmaller = 0;
+        for (int j = 0; j < size; j++)
+        {
+            if ((*v)[j] < (*v)[iSmaller])
+            {
+                iSmaller = j;
+            }
+        }
+        sorted[i] = (*v)[iSmaller];
+        (*v)[iSmaller] = INT_MAX;
+    }
+    (*v) = sorted;
+}
+
+void selectionSortIP(int *v, int size)
+{
+    for (int i = 0; i < (size - 1); i++)
+    {
+        int iSmaller = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (v[j] < v[iSmaller])
+            {
+                iSmaller = j;
+            }
+        }
+        swap(v, i, iSmaller);
+    }
+}
---
+++ library/utils/utils.h
@@ -0,0 +1,6 @@
+void swap(int *v, int i, int j)
+{
+    int temp = v[i];
+    v[i] = v[j];
+    v[j] = temp;
+}
